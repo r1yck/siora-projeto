40c4f59844c15f4d9ee7e6b94bbb1ade7693a89f
r1yck <henriquefontoura.exe@gmail.com>
2026-07-29 22:06:49 -0300

    update: multer.ts

--- siora-backend/src/config/multer.ts
+++ siora-backend/src/config/multer.ts
@@ -2,20 +2,20 @@ import multer from 'multer';
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-// Configura as credenciais obtidas no Dashboard do Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
   api_key: process.env.CLOUDINARY_API_KEY!,
   api_secret: process.env.CLOUDINARY_API_SECRET!,
 });
 
-// Configura o storage do Multer para enviar diretamente ao Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: async (req, file) => {
+    const isRaw = file.mimetype.includes('pdf') || file.mimetype.includes('zip') || file.mimetype.includes('rar');
+
     return {
-      folder: 'siora_uploads', // Nome da pasta criada dentro do seu Cloudinary
-      resource_type: 'auto',   // Identifica automaticamente se é PDF, Imagem, ZIP, etc.
+      folder: 'siora_uploads',
+      resource_type: isRaw ? 'raw' : 'auto',
       public_id: `${Date.now()}-${file.originalname.split('.')[0]}`,
     };
   },
@@ -24,7 +24,7 @@ const storage = new CloudinaryStorage({
 const upload = multer({
   storage,
   limits: {
-    fileSize: 20 * 1024 * 1024, // Limite mantido em 20MB
+    fileSize: 20 * 1024 * 1024,
   },
 });
 
